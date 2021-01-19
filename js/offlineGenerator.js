07a7e19e9ed726a6e90acacd2568f469012716e2
BonyChops <office.kentarou@gmail.com>
2021-01-19 21:18:54 +0900

    Fix: ver. 0.9.5 HTML viewing bug

--- js/offlineGenerator.js
+++ js/offlineGenerator.js
@@ -7,37 +7,45 @@ const downloadOffline = async () => {
     $('.loading-mes').text("ダウンロードする準備をしています...");
     $('.loading-title').text("お待ちください...");
     M.Modal.getInstance($('#loading')).open();
-    let body = await new Promise((resolve, reject) => {
-        fetch("index.html")
-            .then(response => resolve(response.text()))
-            .catch((e) => reject(e));
-    })
-    //console.log(body);
-    const dom = new DOMParser().parseFromString(body, "text/html");
-    console.log(dom.scripts);
-    console.log(dom.getElementsByTagName('i'));
-    const data = [...dom.scripts].concat([...dom.getElementsByTagName('link')]).concat([...dom.getElementsByTagName('i')]).map(async (element) => {
-        const url = element.src !== undefined ? element.src : element.href;
-        const result = element.tagName !== "I" ? {
-            body: element.outerHTML,
-            isJS: element.src !== undefined,
-            src: url,
-            skipped: element.hasAttribute("skip-download"),
-            offlineReplaced: element.hasAttribute("offline-replaced"),
-            jsBody: element.hasAttribute("skip-download") ? false : await new Promise((resolve, reject) => {
-                fetch(url)
-                    .then(response => resolve(response.text()))
-                    .catch(e => reject(e));
-            })
-        } : {
-                skipped: true,
-                offlineReplaced: false,
-                jsBody: false,
-                body: element.outerHTML
-            };
-        return result;
-    });
-    Promise.all(data).then(data => userDownload(body, data));
+    try {
+        let body = await new Promise((resolve, reject) => {
+            fetch("index.html")
+                .then(response => resolve(response.text()))
+                .catch((e) => reject(e));
+        })
+        console.log("show");
+        console.log(body);
+        const dom = new DOMParser().parseFromString(body, "text/html");
+        console.log(dom.scripts);
+        console.log(dom.getElementsByTagName('i'));
+        const data = [...dom.scripts].concat([...dom.getElementsByTagName('link')]).concat([...dom.getElementsByTagName('i')]).map(async (element) => {
+            const url = element.src !== undefined ? element.src : element.href;
+            const result = element.tagName !== "I" ? {
+                body: element.outerHTML,
+                isJS: element.src !== undefined,
+                src: url,
+                skipped: element.hasAttribute("skip-download"),
+                offlineReplaced: element.hasAttribute("offline-replaced"),
+                jsBody: element.hasAttribute("skip-download") ? false : await new Promise((resolve, reject) => {
+                    fetch(url)
+                        .then(response => resolve(response.text()))
+                        .catch(e => reject(e));
+                })
+            } : {
+                    skipped: true,
+                    offlineReplaced: false,
+                    jsBody: false,
+                    body: element.outerHTML
+                };
+            return result;
+        });
+        Promise.all(data).then(data => userDownload(body, data));
+    } catch (e) {
+        console.error(e);
+        M.toast({ html: 'お使いのブラウザは非対応です．別のブラウザでダウンロードしてください．' });
+        M.Modal.getInstance($('#loading')).close();
+    }
+
 }
 
 const sleep = (msc) => { return new Promise(resolve => setTimeout(resolve, msc)) }
@@ -54,20 +62,22 @@ const userDownload = (body, data) => {
             }
         `
     }
-    console.log(data);
-    data.map(el => {
+    data = data.map(el => {
         if (el.jsBody === false) {
             return el;
         }
         el.jsBody.replace('</script>', '{{html "</sc"+"ript>"}}');
         return el;
     })
+    console.log("data");
+    console.log(data);
     const result = data.reduce((acc, val) => {
         if (!val.skipped) {
             //val.jsBody = val.jsBody.replace('$', '$-');
         }
-        console.log(acc.indexOf(val.body));
+        //console.log(acc.indexOf(val.body));
         console.log(val)
+        console.log(( acc.match( /<body>/g ) || [] ).length)
         return acc.split(val.body).join(val.skipped ? (val.offlineReplaced ? `<script>${getOfflineFunctions(versionInfo.version)}</script>` : ' ') : (val.isJS ? `<script type="text/javascript">\n${val.jsBody}\n</script>` : `<style>\n${val.jsBody}\n</style>`))
         //return acc.replace(val.body, val.skipped ? ' ' : (val.isJS ? `<script type="text/javascript">\n${val.jsBody}\n</script>` : `<style>\n${val.jsBody}\n</style>` ))
     }, body).split('\\({ \\rm \\LaTeX }\\)').join("LaTeX");
